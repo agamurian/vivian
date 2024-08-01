205aac86534c4f7ab46a58184d502163eb4d668d
Agamura Ian <agamurian@v2541021.hosted-by-vdsina.ru>
2024-08-01 04:40:32 +0300

    some

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,8 +3,8 @@ import { writable } from 'svelte/store';
 export const hideNav = writable<boolean>(false);
 type themeType = 'light' | 'dark';
 export const theme = writable<themeType>('light');
-export const white = writable('#ffffff');
-export const black = writable('#0a0a0a');
+export const white = writable('#eeeeee');
+export const black = writable('#1a1a1a');
 export const backgroundColor = writable('#888888');
 export const localtitle = writable('');
 
